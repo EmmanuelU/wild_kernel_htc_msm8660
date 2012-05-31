95db2cf91a8e4e3bec24e2be0569c5c1c6d7d7d2
Dmitry Shmidt <dimitrysh@google.com>
2012-05-31 14:06:50 -0700

    Squashed net: wireless: bcmdhd: Update to version 5.90.195.75
    - Fix false PCB-OVERLAP issue
    - Fix simultanious connect request on two P2P devices
    
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    net: wireless: bcmdhd: Combined P2P fixes
    
    - Fix p2p scan
    - Fix p2p processing for channels 12 and 13
    - Fix service discovery
    
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    net: wireless: bcmdhd: Reload FW in case of constant scan failure
    
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    net: wireless: bcmdhd: Reduce priority for dhd_dpc and watchdog
    
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    net: wireless: bcmdhd: Ignore error if "chanspecs" command is not supported
    
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    net: wireless: bcmdhd: Skip inaccurate wl_construct_reginfo() call
    
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    net: wireless: bcmdhd: Return wl_construct_reginfo() call
    
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    net: wireless: bcmdhd: Add wiphyband update for country change
    
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    net: wireless: bcmdhd: Skip country setting if unnecessary
    
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    net: wireless: bcmdhd: Add mutex to wl_update_wiphybands()
    
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    net: wireless: bcmdhd: Update wiphy bands on band change
    
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    net: wireless: bcmdhd: Init locks in dhd_attach() at the beginning
    
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    net: wireless: bcmdhd: Remove country update from wl_update_wiphybands
    
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    net: wireless: bcmdhd: Fix P2P GO hang issue
    
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    net: wireless: bcmdhd: Add info_string param with driver/fw/chip info
    
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    net: wireless: bcmdhd: Adjust driver/fw/chip info format
    
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    net: wireless: bcmdhd: Use proper jiffie-related functions
    
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    Conflicts:
    
    	drivers/net/wireless/bcmdhd/wl_cfg80211.c
    	drivers/net/wireless/bcmdhd/wl_iw.c
    
    net: wireless: bcmdhd: Prevent HT Avail request failure to flood the log
    
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    net: wireless: bcmdhd: Fill station_info packet fields
    
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    net: wireless: bcmdhd: Use correct time from boot for TSF
    
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    net: wireless: bcmdhd: Update to Version 5.90.195.98
    
    Change-Id: I2854300bc03b133d8276bea2f36f4c5253c551a1
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- drivers/net/wireless/bcmdhd/dhd.h
+++ drivers/net/wireless/bcmdhd/dhd.h
@@ -24,7 +24,7 @@
  * software in any way with any other Broadcom software provided under a license
  * other than the GPL, without Broadcom's express prior written consent.
  *
- * $Id: dhd.h 328934 2012-04-23 05:15:42Z $
+ * $Id: dhd.h 344123 2012-07-11 09:33:49Z $
  */
 
 /****************
@@ -51,6 +51,7 @@
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 27)) && defined(CONFIG_HAS_WAKELOCK)
 #include <linux/wakelock.h>
 #endif /* (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 27)) && defined (CONFIG_HAS_WAKELOCK) */
+
 /* The kernel threading is sdio-specific */
 struct task_struct;
 struct sched_param;
@@ -250,7 +251,7 @@ typedef struct dhd_cmn {
 			SMP_RD_BARRIER_DEPENDS(); \
 			while (dhd_mmc_suspend && retry++ != b) { \
 				SMP_RD_BARRIER_DEPENDS(); \
-				wait_event_interruptible_timeout(a, !dhd_mmc_suspend, HZ/100); \
+				wait_event_interruptible_timeout(a, !dhd_mmc_suspend, 1); \
 			} \
 		} while (0)
 	#define DHD_PM_RESUME_WAIT(a) 		_DHD_PM_RESUME_WAIT(a, 200)
@@ -262,7 +263,7 @@ typedef struct dhd_cmn {
 	#define SPINWAIT_SLEEP(a, exp, us) do { \
 		uint countdown = (us) + 9999; \
 		while ((exp) && (countdown >= 10000)) { \
-			wait_event_interruptible_timeout(a, FALSE, HZ/100); \
+			wait_event_interruptible_timeout(a, FALSE, 1); \
 			countdown -= 10000; \
 		} \
 	} while (0)
@@ -430,16 +431,23 @@ extern void dhd_os_sdunlock_sndup_rxq(dhd_pub_t * pub);
 extern void dhd_os_sdlock_eventq(dhd_pub_t * pub);
 extern void dhd_os_sdunlock_eventq(dhd_pub_t * pub);
 extern bool dhd_os_check_hang(dhd_pub_t *dhdp, int ifidx, int ret);
+extern int dhd_os_send_hang_message(dhd_pub_t *dhdp);
+extern int net_os_send_hang_message(struct net_device *dev);
+extern void dhd_set_version_info(dhd_pub_t *pub, char *fw);
 
 #ifdef PNO_SUPPORT
 extern int dhd_pno_enable(dhd_pub_t *dhd, int pfn_enabled);
 extern int dhd_pno_clean(dhd_pub_t *dhd);
 extern int dhd_pno_set(dhd_pub_t *dhd, wlc_ssid_t* ssids_local, int nssid,
                        ushort  scan_fr, int pno_repeat, int pno_freq_expo_max);
+extern int dhd_pno_set_ex(dhd_pub_t *dhd, wl_pfn_t* ssidnet, int nssid,
+				ushort pno_interval, int pno_repeat, int pno_expo_max, int pno_lost_time);
 extern int dhd_pno_get_status(dhd_pub_t *dhd);
 extern int dhd_dev_pno_reset(struct net_device *dev);
 extern int dhd_dev_pno_set(struct net_device *dev, wlc_ssid_t* ssids_local,
                            int nssid, ushort  scan_fr, int pno_repeat, int pno_freq_expo_max);
+extern int dhd_dev_pno_set_ex(struct net_device *dev, wl_pfn_t* ssidnet, int nssid,
+				ushort	pno_interval, int pno_repeat, int pno_expo_max, int pno_lost_time);
 extern int dhd_dev_pno_enable(struct net_device *dev,  int pfn_enabled);
 extern int dhd_dev_get_pno_status(struct net_device *dev);
 #endif /* PNO_SUPPORT */
@@ -515,6 +523,8 @@ extern int dhd_bus_membytes(dhd_pub_t *dhdp, bool set, uint32 address, uint8 *da
 extern void dhd_print_buf(void *pbuf, int len, int bytes_per_line);
 extern bool dhd_is_associated(dhd_pub_t *dhd, void *bss_buf, int *retval);
 extern uint dhd_bus_chip_id(dhd_pub_t *dhdp);
+extern uint dhd_bus_chiprev_id(dhd_pub_t *dhdp);
+extern uint dhd_bus_chippkg_id(dhd_pub_t *dhdp);
 
 #if defined(KEEP_ALIVE)
 extern int dhd_keep_alive_onoff(dhd_pub_t *dhd);
@@ -602,6 +612,8 @@ extern uint dhd_pktgen_len;
 #define MOD_PARAM_PATHLEN	2048
 extern char fw_path[MOD_PARAM_PATHLEN];
 extern char nv_path[MOD_PARAM_PATHLEN];
+
+#define MOD_PARAM_INFOLEN	512
 
 #ifdef SOFTAP
 extern char fw_path2[MOD_PARAM_PATHLEN];
