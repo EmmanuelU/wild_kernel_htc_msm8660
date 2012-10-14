175d0b52bea0ebceb907409c0431c71a06cbcc31
Chad Goodman <cgoodman@hp-tablet.(none)>
2012-10-14 10:39:22 -0700

    GPU: dont OC by default, allow SYSFS contol over max clock
    Conflicts:
    	arch/arm/mach-msm/devices-msm8x60.c

--- include/linux/msm_kgsl.h
+++ include/linux/msm_kgsl.h
@@ -154,6 +154,7 @@ struct kgsl_version {
 struct kgsl_device_platform_data {
 	struct kgsl_pwrlevel pwrlevel[KGSL_MAX_PWRLEVELS];
 	int init_level;
+	int max_level;
 	int num_levels;
 	int (*set_grp_async)(void);
 	unsigned int idle_timeout;
