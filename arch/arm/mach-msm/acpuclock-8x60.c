c6a36023618d1328dead40e740b6f2c698ee1be7
Steve Verhagen <scverhagen@gmail.com>
2012-10-23 12:36:10 -0500

    acupclock: undervolting 96mhz to 400000 is still stable

--- arch/arm/mach-msm/acpuclock-8x60.c
+++ arch/arm/mach-msm/acpuclock-8x60.c
@@ -291,7 +291,7 @@ static struct clkctl_acpu_speed acpu_freq_tbl_nom[] = {
 
 /* SCPLL frequencies = 2 * 27 MHz * L_VAL */
 static struct clkctl_acpu_speed acpu_freq_tbl_fast[] = {
-  { {1, 1},  96000,  ACPU_PLL_8, 3, 1, 0, 0,    L2(1),   500000, 0x03006000},
+  { {1, 1},  96000,  ACPU_PLL_8, 3, 1, 0, 0,    L2(1),   400000, 0x03006000},
   /* MAX_AXI row is used to source CPU cores and L2 from the AFAB clock. */
   { {0, 0},  MAX_AXI, ACPU_AFAB,  1, 0, 0, 0,    L2(0),   750000, 0x03006000},
   { {1, 1},  196000,  ACPU_PLL_8, 3, 0, 0, 0,    L2(1),   750000, 0x03006000},
