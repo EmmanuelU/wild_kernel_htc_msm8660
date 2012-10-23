961ca1206a3683ca71cc639751aa95773e969826
Steve Verhagen <scverhagen@gmail.com>
2012-10-22 21:37:58 -0500

    more work on release tools

--- releasetools/make-changelog.sh
+++ releasetools/make-changelog.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+if [ -e "changelog.txt" ]
+then
+	rm changelog.txt
+fi
+
 git log --after="yesterday" > changelog.txt
---
+++ releasetools/make-zip.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#***CREATE ANYKERNEL PACKAGE***:
+
+DATE=$1
+
+#append NO-CHANGES to archive name if there is not git commit log:
+if [ -e "changelog.txt" ]
+then
+	DATE=$DATE
+else
+	DATE=$DATE-NO-CHANGES
+fi
+
+if [ -e "../arch/arm/boot/zImage" ]
+then
+	cp -f ../arch/arm/boot/zImage anykernel/kernel/zImage
+else
+	DATE=$DATE-FAIL
+fi
+
+if [ -e "../drivers/net/wireless/bcmdhd/bcmdhd.ko" ]
+then
+	cp -f ../drivers/net/wireless/bcmdhd/bcmdhd.ko anykernel/system/lib/modules/bcmdhd.ko
+else
+	DATE=$DATE-NOWIFI
+fi
+
+#copy new changelog to ANYKERNEL folder:
+cp -f changelog.txt anykernel/changelog.txt
+
+cd anykernel
+zip -r9 ../kernel-zip/$DATE.zip *
+
+cd ..
