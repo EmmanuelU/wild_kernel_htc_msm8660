ad6739bb23cbfde256cba8ab5e69954fdb9d5daf
Steve Verhagen <scverhagen@gmail.com>
2012-11-29 08:17:47 -0600

    cpufreq: fix compiling with intellidemand [lomarb]

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -90,7 +90,7 @@ static void set_cpu_work(struct work_struct *work)
 #endif
 
 #ifdef CONFIG_CPU_FREQ_GOV_INTELLIDEMAND
-extern bool lmf_screen_state;
+static bool lmf_screen_state;
 #endif
 
 static void msm_cpu_early_suspend(struct early_suspend *h)
