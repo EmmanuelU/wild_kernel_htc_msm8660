dd7e295c11152aba74964b6cf58ea171b49205a8
homeslice976 <homeslice976@gmail.com>
2013-02-04 09:31:25 -0500

    Added qdsp6v3 [homeslice976] (thanks EmmanuelU for the find)
    Conflicts:
    	arch/arm/mach-msm/board-pyramid-audio.c
    
    board-doubleshot-audio.c: Adjust sound frequency back to safe doubleshot levels

--- arch/arm/mach-msm/board-doubleshot-audio.c
+++ arch/arm/mach-msm/board-doubleshot-audio.c
@@ -23,15 +23,17 @@
 #include <mach/gpio.h>
 #include <mach/dal.h>
 #include <mach/tpa2051d3.h>
-#include <mach/qdsp6v2_1x/snddev_icodec.h>
-#include <mach/qdsp6v2_1x/snddev_ecodec.h>
-#include <mach/qdsp6v2_1x/snddev_hdmi.h>
+#include <mach/qdsp6v3/snddev_icodec.h>
+#include <mach/qdsp6v3/snddev_ecodec.h>
+#include <mach/qdsp6v3/snddev_hdmi.h>
+#include <mach/qdsp6v3/apr_audio.h>
+#include <mach/qdsp6v3/q6asm.h>
 #include <mach/htc_acoustic_8x60.h>
 
 #include "board-doubleshot.h"
 #include "board-doubleshot-audio-data.h"
 
-#include <mach/qdsp6v2_1x/audio_dev_ctl.h>
+#include <mach/qdsp6v3/audio_dev_ctl.h>
 
 static struct mutex bt_sco_lock;
 static struct mutex mic_lock;
@@ -502,7 +504,6 @@ void doubleshot_aic3254_set_mode(int config, int mode)
 {
 	aic3254_set_mode(config, mode);
 }
-
 
 static struct q6v2audio_aic3254_ops aops = {
        .aic3254_set_mode = doubleshot_aic3254_set_mode,
