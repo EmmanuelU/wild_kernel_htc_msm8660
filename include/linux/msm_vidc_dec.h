4aadf859114089caada166b2381e5e6aef9760a1
tbalden <illespal@gmail.com>
2013-03-24 22:21:41 +0100

    vidc: updating to new version
    Change-Id: Ifc3c54cf97376058f043d80277e5251655f3be05

--- include/linux/msm_vidc_dec.h
+++ include/linux/msm_vidc_dec.h
@@ -513,11 +513,13 @@ struct vdec_framesize {
 	uint32_t   right;
 	uint32_t   bottom;
 };
+
 struct vdec_aspectratioinfo {
- uint32_t aspect_ratio;
- uint32_t par_width;
- uint32_t par_height;
+	uint32_t aspect_ratio;
+	uint32_t par_width;
+	uint32_t par_height;
 };
+
 struct vdec_output_frameinfo {
 	void __user *bufferaddr;
 	size_t offset;
@@ -529,7 +531,7 @@ struct vdec_output_frameinfo {
 	void *input_frame_clientdata;
 	struct vdec_framesize framesize;
 	enum vdec_interlaced_format interlaced_format;
- struct vdec_aspectratioinfo aspect_ratio_info;
+	struct vdec_aspectratioinfo aspect_ratio_info;
 };
 
 union vdec_msgdata {
