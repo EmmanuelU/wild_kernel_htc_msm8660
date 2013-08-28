0ed85595c67c10c2c9d591620f73aa9de01c798b
Emmanuel Utomi <emmanuelutomi@gmail.com>
2013-08-27 19:41:12 -0500

    Build Script V2
    Conflicts:
    	build
    	flashable/zip/system/lib/modules/bcmdhd.ko
    	flashable/zip/system/lib/modules/ntfs.ko

--- flashable/zip/kernel/mkbootimg.sh
+++ flashable/zip/kernel/mkbootimg.sh
@@ -1,6 +1,8 @@
 #!/sbin/sh
 echo \#!/sbin/sh > /tmp/createnewboot.sh
-echo /tmp/mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline \"$(cat /tmp/boot.img-cmdline)\" --base $(cat /tmp/boot.img-base) --output /tmp/newboot.img >> /tmp/createnewboot.sh
+echo /tmp/mkbootimg --kernel /tmp/zImage --ramdisk /tmp/ramdisks/$1.gz --cmdline "no_console_suspend=1" --base 0x48000000 --output /tmp/boot.img >> /tmp/createnewboot.sh
+rm -rf /sdcard/WildKernel-boot.img
+cp /tmp/boot.img /sdcard/WildKernel-boot.img
 chmod 777 /tmp/createnewboot.sh
 /tmp/createnewboot.sh
 return $?
