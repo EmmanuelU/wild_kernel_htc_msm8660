a8b0172636f7b2b087dacf6d24dcc8641448c1ec
Emmanuel Utomi <emmanuelutomi@gmail.com>
2015-04-01 19:22:26 -0500

    Revert "cpufreq: Cleanup cpu_gov_sync"
    This reverts commit e0bfb009a0aaf9ff64f2d5c13ef0451de6321d16.
    
    Conflicts:
    	flashable/tools/updatewild.sh

--- flashable/tools/updatewild.sh
+++ flashable/tools/updatewild.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 #DO NOT EDIT FILE - AUTO GENERATED FOR doubleshot
 latestversion=001
+<<<<<<< HEAD
 latestdate=141118
 latestdateliteral='November 18 2014'
 latestDL=http://forum.xda-developers.com/showthread.php?t=2551715
+=======
+latestdate=141013
+latestdateliteral='October 13 2014'
+latestDL=http://forum.xda-developers.com/showthread.php?t=2765196
+>>>>>>> parent of e0bfb00... cpufreq: Cleanup cpu_gov_sync
 input="$1"
 if [[ -z "$input" ]]
 	then
